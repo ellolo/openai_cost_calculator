721deb68b32e77cb8c4489d1eee0d856445498ad
Marco <marco.pennacchiotti@gmail.com>
2025-11-13 13:14:34 +0100

    add license and new deps

--- setup.py
+++ setup.py
@@ -1,22 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="openai_cost_calculator",
-    version="1.1.1",
-    description="A library to estimate OpenAI API costs based on token usage.",
-    author="Orkun Kınay, Murat Barkın Kınay",
-    author_email="orkunkinay@sabanciuniv.edu",
-    url="https://github.com/orkunkinay/openai_cost_calculator", 
+    name="openai_cost_calculator_with_langchain",
+    version="1.0.0",
+    description="A library to estimate OpenAI API costs based on token usage, integrating with Langchain. This is a fork from v1.1.1 of openai_cost_calculator by Orkun Kınay and Murat Barkın Kınay (https://github.com/orkunkinay/openai_cost_calculator).",
+    author="Marco Pennacchiotti (forked from openai_cost_calculator by Orkun Kınay and Murat Barkın Kınay)",
     packages=find_packages(),
     include_package_data=True, 
     package_data={
         "openai_cost_calculator": ["data/gpt_pricing_data.csv"],
     },
-    install_requires=["requests"],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.8',
+    python_requires='>=3.8.1',
 )
